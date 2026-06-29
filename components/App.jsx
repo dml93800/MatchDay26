b5ef4674f734e87dff9297730aa72aad8d07f9e1
dml93800 <djameljvv@gmail.com>
2026-06-29 09:07:28 +0200

    debug matchcard

--- components/App.jsx
+++ components/App.jsx
@@ -1,9 +1,11 @@
 import Auth from './Auth'
+import MatchCard from './MatchCard'
 
 export default function App() {
   return (
     <div style={{ color: 'white', padding: 20, background: '#050508', minHeight: '100vh' }}>
-      <h1>Auth importé : {Auth ? '✅ OK' : '❌ undefined'}</h1>
+      <h1>Auth : {Auth ? '✅' : '❌'}</h1>
+      <h1>MatchCard : {MatchCard ? '✅' : '❌'}</h1>
     </div>
   )
 }
